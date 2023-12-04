3ad2d392659f117c395b16dd8ae5446247f5e4de
maryUKU <112956723+maryuka@users.noreply.github.com>
2023-12-04 17:17:26 +0900

    [fix] aboutのスライドショー表示が遅かったためネイティブcss で書き直し

--- components/About.vue
+++ components/About.vue
@@ -1,24 +1,5 @@
 <script setup lang="ts">
-
-const items = [
-    {
-        src: '/img/like1.webp',
-    },
-    {
-        src: '/img/like2.webp',
-    },
-    {
-        src: '/img/like3.webp',
-    },
-    {
-        src: '/img/like4.webp',
-    },
-    {
-        src: '/img/like5.webp',
-    },
-]
-
-const { t,locale } = useI18n()
+const { t, locale } = useI18n()
 const title_txt = t(`About.title`)
 
 </script>
@@ -35,8 +16,9 @@ const title_txt = t(`About.title`)
             }">
                 <v-row no-gutters class="profile mt-16" justify="center">
                     <v-col class="txt_box" cols="12" lg="4">
-                        <h1 v-text="$t(`About.profile.title`)"  style="white-space: pre-wrap;" class="profile_title is_en"></h1>
-                        <p  v-text="$t(`About.profile.text`)"  style="white-space: pre-wrap;" class="txt mt-10"></p>
+                        <h1 v-text="$t(`About.profile.title`)" style="white-space: pre-wrap;" class="profile_title is_en">
+                        </h1>
+                        <p v-text="$t(`About.profile.text`)" style="white-space: pre-wrap;" class="txt mt-10"></p>
                     </v-col>
                     <v-spacer></v-spacer>
                     <v-col class="img_box mt-10  d-flex justify-center" cols="12" lg="6">
@@ -52,16 +34,17 @@ const title_txt = t(`About.title`)
             }">
                 <v-row no-gutters class="profile is_reverse mt-16" justify="center">
                     <v-col class="txt_box" cols="12" lg="4">
-                        <h1  v-text="$t(`About.likes.title`)"  style="white-space: pre-wrap;" class="profile_title is_en"></h1>
-                        <p  v-text="$t(`About.likes.text`)" style="white-space: pre-wrap;" class="txt mt-10"></p>
+                        <h1 v-text="$t(`About.likes.title`)" style="white-space: pre-wrap;" class="profile_title is_en">
+                        </h1>
+                        <p v-text="$t(`About.likes.text`)" style="white-space: pre-wrap;" class="txt mt-10"></p>
                     </v-col>
                     <v-spacer></v-spacer>
                     <v-col class="img_box mt-10 d-flex justify-center" cols="12" lg="6">
-                        <v-carousel cycle color="#ffffff" :show-arrows="false" :hide-delimiter-background="true"
-                            :hide-delimiters="true">
-                            <v-carousel-item v-for="(item, i) in items" aspect-ratio="1.5" :key="i" :src="item.src"
-                                height="52vw" max-height="300" :draggable="false" cover></v-carousel-item>
-                        </v-carousel>
+                        <div class="img_frame">
+                            <div class="img_01"></div>
+                            <div class="img_02"></div>
+                            <div class="img_03"></div>
+                        </div>
                     </v-col>
                 </v-row>
             </div>
@@ -70,14 +53,6 @@ const title_txt = t(`About.title`)
 </template>
 
 <style scoped>
-.v-carousel {
-    height: auto !important;
-}
-
-.v-carousel-item :deep(img) {
-    border-radius: 24px;
-}
-
 .sheet {
     background-color: #E5EAF6;
     height: 100%;
@@ -98,6 +73,111 @@ const title_txt = t(`About.title`)
 .img {
     width: 450px;
     height: 300px;
+}
+
+.img_frame {
+    position: relative;
+    width: 450px;
+    height: 300px;
+    margin: 0 auto;
+}
+
+.img_01,
+.img_02,
+.img_03 {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 450px;
+    height: 300px;
+    border-radius: 24px;
+    background-size: cover;
+    background-repeat: no-repeat;
+}
+
+.img_01 {
+    background-image: url('/img/like1.webp');
+    animation: slide-animation-01 24s infinite;
+}
+
+.img_02 {
+    background-image: url('/img/like2.webp');
+    animation: slide-animation-02 24s infinite;
+}
+
+.img_03 {
+    background-image: url('/img/like3.webp');
+    animation: slide-animation-03 24s infinite;
+}
+
+@keyframes slide-animation-01 {
+    0% {
+        opacity: 1;
+    }
+
+    30% {
+        opacity: 1;
+    }
+
+    40% {
+        opacity: 0;
+    }
+
+    90% {
+        opacity: 0
+    }
+
+    100% {
+        opacity: 1;
+    }
+}
+
+@keyframes slide-animation-02 {
+    0% {
+        opacity: 0;
+    }
+
+    30% {
+        opacity: 0;
+    }
+
+    40% {
+        opacity: 1;
+    }
+
+    60% {
+        opacity: 1;
+    }
+
+    70% {
+        opacity: 0;
+    }
+
+    100% {
+        opacity: 0;
+    }
+}
+
+@keyframes slide-animation-03 {
+    0% {
+        opacity: 0;
+    }
+
+    60% {
+        opacity: 0;
+    }
+
+    70% {
+        opacity: 1;
+    }
+
+    90% {
+        opacity: 1;
+    }
+
+    100% {
+        opacity: 0;
+    }
 }
 
 @media screen and (max-width: 1280px) {
