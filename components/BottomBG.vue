c5c2380313f40933f8f4d2b2a5642ac374e236d0
maryUKU <112956723+maryuka@users.noreply.github.com>
2023-12-04 16:43:20 +0900

    [chore]モバイルでのサイズ変更

--- components/BottomBG.vue
+++ components/BottomBG.vue
@@ -1,21 +1,32 @@
 <template>
-    <v-parallax class="bottom-background-image" src="/img/bottom.webp" scale="0.6"></v-parallax>
+    <v-parallax class="bottom-background-image" src="/img/bottom.webp"></v-parallax>
 </template>
 
-<style  scoped>
+<style scoped>
 .bottom-background-image {
     width: 100%;
     height: 300px;
 }
 
 .bottom-background-image :deep(img) {
-    min-width: 80%;
+    max-width: 80%;
 }
+
 
 @media screen and (max-width: 1280px) {
     .bottom-background-image {
         width: 100%;
         height: 30vw;
+    }
+
+    .bottom-background-image :deep(img) {
+        max-width: 90%;
+    }
+}
+
+@media screen and (max-width:960px) {
+    .bottom-background-image :deep(img) {
+        max-width: 80%;
     }
 }
 </style>
