e9f78a37730380df932d0be7e0254d2edc924ac2
maryuka <mary.ukiuki@gmail.com>
2024-12-11 17:38:14 +0900

    フッターコンポーネントを追加し、著作権情報を表示

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,1 +1,16 @@
-<template></template>
+<template>
+  <footer class="footer">
+    <div class="content py-6">
+      <p class="text-center text-caption">
+        &copy; Copyright 2024 Yuka All rights reserved.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<style scoped>
+.footer {
+  background-color: #e5eaf6;
+  color: #333;
+}
+</style>
